02702c33e1c71999faa7f23ed17ff21827d59f48
Ernest Warzocha <ernestwarzocha@gmail.com>
2024-09-12 18:41:13 +0200

    refactor

--- utils.py
+++ utils.py
@@ -21,3 +21,33 @@ def set_bit(value, bit):
 
 # def get_bits():
 #     pass
+
+def get_message_length(image):
+    length = 0
+
+    for i in range(16):
+        bit = is_even(image[i * 3])
+        length = (length << 1) | bit
+
+    return length
+
+def get_message(image, length):
+    message = ""
+
+    current_value = 16 * 3
+
+    for char_index in range(length):
+
+        char = 0
+
+        for _ in range(8):
+
+            bit = is_even(image[current_value])
+            char = (char << 1) | bit
+
+            current_value += 3
+
+        message += chr(char)
+
+    return message
+    
