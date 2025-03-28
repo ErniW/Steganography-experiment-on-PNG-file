92d8f12e33b1af49a4f330be8a0c9fb046d9f58b
Ernest Warzocha <ernestwarzocha@gmail.com>
2025-03-28 17:51:48 +0100

    backup
    separated cpp & bmp

---
+++ bmp_cpp/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+#include <String>
+
+#include <openssl/aes.h>
+#include <openssl/rand.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+
+int main(){
+    
+}
